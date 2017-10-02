98d919548b71462ffa74de756ec9aa8e09238386
baivong <Zzbaivong@gmail.com>
2017-10-02 22:39:18 +0700

    Autodetect

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,7 @@
 
         beautify = document.getElementById('beautify'),
         highlight = document.getElementById('highlight'),
+        auto = document.getElementById('auto'),
 
         copyjs = document.getElementById('copyjs'),
         redecode = document.getElementById('redecode'),
@@ -91,11 +92,37 @@
             });
         }, 250),
 
+        detect = function (source) {
+            var type = '';
+            if (/^[\s\n]*var\s_\d{4}\;[\s\n]*var\s_\d{4}\s?=/.test(source)) {
+                type = '_numberencode';
+            } else if (source.indexOf("/｀ｍ´）ﾉ ~┻━┻   //*´∇｀*/ ['_'];") !== -1) {
+                type = 'aaencode';
+            } else if (source.indexOf('$={___:++$,$$$$:(![]+"")[$]') !== -1) {
+                type = 'jjencode';
+            } else if (source.replace(/[\[\]\(\)\!\+]/gm, '').trim() === '') {
+                type = 'jsfuck';
+            } else if (source.indexOf(' ') === -1 && (source.indexOf('%2') !== -1 || source.replace(/[^%]+/g, '').length > 3)) {
+                type = 'urlencode';
+            } else if (source.indexOf('eval(') !== -1) {
+                type = 'evalencode';
+            } else if (/^[\s\n]*var\s_0x\w+\s?=\s?\["/.test(source)) {
+                type = 'arrayencode';
+            }
+
+            document.querySelector('.magic-radio:checked').checked = false;
+            document.querySelector('.magic-radio[value="' + type + '"]').checked = true;
+
+            return type;
+        },
+
         decode = debounce(function () {
             var source = input.value.trim(),
                 packer = bvDecode.encode.value;
 
             if (source === '') return;
+            if (auto.checked) packer = detect(source);
+
             if (packer === 'nicify') return;
             if (packer === '') {
                 output.value = source;
@@ -108,7 +135,12 @@
                 workerDecode.addEventListener('message', function (e) {
                     output.value = e.data;
                     if (!beautify.checked && !highlight.checked) stopEffect();
-                    format();
+
+                    if (auto.checked && input.value !== output.value) {
+                        redecode.onclick();
+                    } else {
+                        format();
+                    }
                 });
             }
 
@@ -129,6 +161,14 @@
 
     beautify.onchange = format;
     highlight.onchange = format;
+
+    auto.onchange = function () {
+        for (var i = 0; i < encode.length; i++) {
+            if (encode[i].value === 'nicify') continue;
+            encode[i].disabled = auto.checked;
+        }
+        decode();
+    };
 
     copyjs.onmouseout = function () {
         textreset();
@@ -154,6 +194,9 @@
     clear.onclick = function () {
         view.textContent = 'Please choose a right encoding type!';
         stopEffect();
+        setTimeout(function() {
+            auto.onchange();
+        }, 0);
 
         if (workerDecode) {
             workerDecode.terminate();
