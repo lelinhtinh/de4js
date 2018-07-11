5176507d59f58025d7e78e123fd82865dd888143
lelinhtinh <Zzbaivong@gmail.com>
2018-07-11 09:51:08 +0700

    Update for Greasemonkey 4. Fix URL does not match.

--- userscript/de4js_helper.user.js
+++ userscript/de4js_helper.user.js
@@ -2,18 +2,20 @@
 // @name         de4js helper
 // @namespace    https://baivong.github.io/de4js/
 // @description  Enable Unreadable option in de4js
-// @version      1.1.0
+// @version      1.2.0
 // @icon         https://i.imgur.com/CJ5MfxV.png
 // @author       Zzbaivong
 // @license      MIT
-// @match        https://baivong.github.io/de4js/
+// @match        https://lelinhtinh.github.io/de4js/
 // @include      http://127.0.0.1:4000/de4js/
 // @include      http://localhost:4000/de4js/
+// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js?v=a834d46
 // @noframes
 // @connect      jsnice.org
-// @supportURL   https://github.com/baivong/de4js/issues
+// @supportURL   https://github.com/lelinhtinh/de4js/issues
 // @run-at       document-idle
 // @grant        GM_xmlhttpRequest
+// @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
 (function () {
@@ -28,9 +30,9 @@
         if (input.value.trim() === '') return;
 
         view.classList.add('waiting');
-        GM_xmlhttpRequest({
+        GM.xmlHttpRequest({
             method: 'POST',
-            url: 'http://www.jsnice.org/beautify?pretty=0&rename=1&types=0&suggest=0',
+            url: 'http://jsnice.org/beautify?pretty=0&rename=1&types=0&packers=0&transpile=0&suggest=0',
             responseType: 'json',
             data: input.value,
             onload: function (response) {
