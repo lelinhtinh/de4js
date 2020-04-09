a5d4c9fc319288f410e00d44aad9eb5ca0ca81e0
lelinhtinh <lelinhtinh2013@gmail.com>
2020-04-09 22:36:57 +0700

    Wrap beautify and highlight in try-catch blocks

--- assets/js/worker/format.js
+++ assets/js/worker/format.js
@@ -12,22 +12,29 @@
 self.addEventListener('message', function (e) {
     var source = e.data.source;
 
-    self._window = self.window;
-    self.window = {};
+    try {
+        self._window = self.window;
+        self.window = {};
 
-    self.importScripts('{{ "/assets/js/lib/js-beautify/beautify.min.js" | relative_url }}');
+        self.importScripts('{{ "/assets/js/lib/js-beautify/beautify.min.js" | relative_url }}');
 
-    source = self.window.js_beautify(source, {
-        unescape_strings: true,
-        jslint_happy: true
-    });
+        source = self.window.js_beautify(source, {
+            unescape_strings: true,
+            jslint_happy: true
+        });
 
-    self.window = self._window;
+        self.window = self._window;
+    } catch (err) {
+        console.log(err);
+    }
 
+    try {
+        self.importScripts('{{ "/assets/js/lib/highlight-js/highlight.min.js" | relative_url }}');
 
-    self.importScripts('{{ "/assets/js/lib/highlight-js/highlight.min.js" | relative_url }}');
-
-    source = self.hljs.highlight('javascript', source).value;
+        source = self.hljs.highlight('javascript', source).value;
+    } catch (err) {
+        console.log(err);
+    }
 
     self.postMessage(source);
 });
