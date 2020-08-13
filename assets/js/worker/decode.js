62742a023ce3e1c2a4fb8386029b35b55a7ca011
lelinhtinh <lelinhtinh2013@gmail.com>
2020-08-13 20:33:18 +0700

    Refactor decode methods

--- assets/js/worker/decode.js
+++ assets/js/worker/decode.js
@@ -16,80 +16,74 @@ self.addEventListener('message', function (e) {
     var source = e.data.source,
         packer = e.data.packer;
 
-    if (packer === 'evalencode') {
-        self._eval = self.eval;
-        self.eval = function (_evalsource) {
-            source = _evalsource;
-        };
-        try {
-            self._eval(source);
-        } catch (err) {
-            console.log(err);
-        }
-        self.eval = self._eval;
-    } else if (packer === '_numberencode') {
-        try {
+    var methods = {
+        evalencode: function () {
+            self._eval = self.eval;
+            self.eval = function (_evalsource) {
+                return _evalsource;
+            };
+            setTimeout(function () {
+                self.eval = self._eval;
+            }, 0);
+            return self._eval(source);
+        },
+        _numberencode: function () {
             var patt = /_\d{4}\((_\d{4})\);\}/,
                 _numbersource = source;
 
-            if (patt.test(_numbersource)) {
-                _numbersource = _numbersource.replace(/var\s/g, 'this.');
-                _numbersource = _numbersource.replace(/function\s(_\d{4})\(/, 'this.$1=function(');
-                _numbersource = _numbersource.replace(patt, 'self.sourceNumberEncodeZz=$1;};');
+            if (!patt.test(_numbersource)) throw 'Not matched';
 
-                _numbersource = '(function(){' + _numbersource + '})();';
-                eval(_numbersource);
+            _numbersource = _numbersource.replace(/var\s/g, 'this.');
+            _numbersource = _numbersource.replace(/function\s(_\d{4})\(/, 'this.$1=function(');
+            _numbersource = _numbersource.replace(patt, 'self.sourceNumberEncodeZz=$1;};');
 
-                source = self.sourceNumberEncodeZz;
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'arrayencode') {
-        try {
+            _numbersource = '(function(){' + _numbersource + '})();';
+            eval(_numbersource);
+
+            return self.sourceNumberEncodeZz;
+        },
+        arrayencode: function () {
             var pattarr = /[\s\n]*var\s+([\w\d_$]+)\s*=\s*\[.*?\];/,
                 _var = source.match(pattarr);
 
-            if (_var && _var.length === 2) {
-                var _name = _var[1],
-                    _code = source.replace(pattarr, ''),
+            if (!_var || _var.length !== 2) throw 'Not matched';
 
-                    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Escaping
-                    quote = function (s, q) {
-                        return s.replace(new RegExp('[\\n*+?^${}()|[\\]\\\\' + q + ']', 'g'), '\\$&');
-                    },
+            var _name = _var[1],
+                _code = source.replace(pattarr, ''),
 
-                    pattkey = new RegExp(_name.replace(/\$/g, '\\$') + '\\[(\\d+)\\]', 'g');
+                quote = function (s, q) {
+                    return s.replace(new RegExp('[\\n*+?^${}()|[\\]\\\\' + q + ']', 'g'), '\\$&');
+                },
 
-                _var = _var[0].replace(/[\s\S]*?\[/, '[');
-                _var = eval(_var);
+                pattkey = new RegExp(_name.replace(/\$/g, '\\$') + '\\[(\\d+)\\]', 'g');
 
-                _code = _code.split(';');
-                _code = _code.map(function (piece) {
-                    piece.replace(pattkey, function (key, index) {
-                        var item = _var[index],
-                            q = item.indexOf('"') !== -1 ? "'" : '"';
+            _var = _var[0].replace(/[\s\S]*?\[/, '[');
+            _var = eval(_var);
 
-                        piece = piece.replace(key, q + quote(item, q) + q);
+            _code = _code.split(';');
+            _code = _code.map(function (piece) {
+                piece.replace(pattkey, function (key, index) {
+                    var item = _var[index],
+                        q = item.indexOf('"') !== -1 ? "'" : '"';
 
-                        return piece;
-                    });
+                    piece = piece.replace(key, q + quote(item, q) + q);
 
                     return piece;
                 });
-                _code = _code.join(';');
 
-                _code = _code.replace(/(\[("|')((?!\d)[a-z_\d$]+)("|')\])/gi, '.$3 ');
-                source = _code;
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'jsfuck') {
-        try {
+                return piece;
+            });
+            _code = _code.join(';');
+
+            _code = _code.replace(/(\[("|')((?!\d)[a-z_\d$]+)("|')\])/gi, '.$3 ');
+
+            return _code;
+        },
+        jsfuck: function () {
             var pattfuck = /\)(\(\)[\s\n]*)$/,
                 pattanon = /^[\s\n]*function\sanonymous\([\s\n]+\)\s\{[\s\n]+/,
                 _fucksource = source;
+
             if (pattfuck.test(source)) _fucksource = _fucksource.replace(pattfuck, ')');
             _fucksource = eval(_fucksource + '.toString()');
 
@@ -98,58 +92,42 @@ self.addEventListener('message', function (e) {
                 _fucksource = _fucksource.replace(/[\s\n]+\}[\s\n]*$/, '');
             }
 
-            source = _fucksource;
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'aaencode') {
-        try {
+            return _fucksource;
+        },
+        aaencode: function () {
             self.importScripts('{{ "/assets/js/lib/cat-in-136/aadecode.js" | relative_url }}');
-
-            source = AADecode.decode(source);
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'jjencode') {
-        try {
+            return AADecode.decode(source);
+        },
+        jjencode: function () {
             self.importScripts('{{ "/assets/js/lib/decoder-jjencode/jjdecode.js" | relative_url }}');
-
-            source = JJdecode.decode(source);
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'urlencode') {
-        try {
+            return JJdecode.decode(source);
+        },
+        urlencode: function () {
             self.importScripts('{{ "/assets/js/lib/js-beautify/unpackers/urlencode_unpacker.js" | relative_url }}');
-
-            if (Urlencoded.detect(source)) source = Urlencoded.unpack(source);
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'p_a_c_k_e_r') {
-        try {
+            if (Urlencoded.detect(source)) return Urlencoded.unpack(source);
+            throw 'Not matched';
+        },
+        p_a_c_k_e_r: function () {
             self.importScripts('{{ "/assets/js/lib/js-beautify/unpackers/p_a_c_k_e_r_unpacker.js" | relative_url }}');
-
-            if (P_A_C_K_E_R.detect(source)) source = P_A_C_K_E_R.unpack(source);
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'javascriptobfuscator') {
-        try {
+            if (P_A_C_K_E_R.detect(source)) return P_A_C_K_E_R.unpack(source);
+            throw 'Not matched';
+        },
+        javascriptobfuscator: function () {
             self.importScripts('{{ "/assets/js/lib/js-beautify/unpackers/javascriptobfuscator_unpacker.js" | relative_url }}');
-
-            if (JavascriptObfuscator.detect(source)) source = JavascriptObfuscator.unpack(source);
-        } catch (err) {
-            console.log(err);
-        }
-    } else if (packer === 'myobfuscate') {
-        try {
+            if (JavascriptObfuscator.detect(source)) return JavascriptObfuscator.unpack(source);
+            throw 'Not matched';
+        },
+        myobfuscate: function () {
             self.importScripts('{{ "/assets/js/lib/js-beautify/unpackers/myobfuscate_unpacker.js" | relative_url }}');
-
-            if (MyObfuscate.detect(source)) source = MyObfuscate.unpack(source);
-        } catch (err) {
-            console.log(err);
+            if (MyObfuscate.detect(source)) return MyObfuscate.unpack(source);
+            throw 'Not matched';
         }
+    };
+
+    try {
+        source = methods[packer]();
+    } catch (err) {
+        console.log(err);
     }
 
     self.postMessage(source);
