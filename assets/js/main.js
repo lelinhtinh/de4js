a5a0c30b4ce78edc86d572306007f6dc94bc15b4
Whoami021 <whoamiclay021@gmail.com>
2020-11-01 10:34:14 +0330

    Update main.js to add saveFile function
    Update main.js to add saveFile function  for download button in default.html file

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,37 @@
       if (callNow) func.apply(context, args);
     };
   }
+  
+  
+  
+  // add download button https://github.com/whoami021
+  
+  function saveTextAsFile(textToWrite, fileNameToSaveAs)
+    {
+    	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'}); 
+    	var downloadLink = document.createElement("a");
+    	downloadLink.download = fileNameToSaveAs;
+    	downloadLink.innerHTML = "Download File";
+    	if (window.webkitURL != null)
+    	{
+    		// Chrome allows the link to be clicked
+    		// without actually adding it to the DOM.
+    		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    	}
+    	else
+    	{
+    		// Firefox requires the link to be added to the DOM
+    		// before it can be clicked.
+    		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    		downloadLink.onclick = destroyClickedElement;
+    		downloadLink.style.display = "none";
+    		document.body.appendChild(downloadLink);
+    	}
+    
+    	downloadLink.click();
+    }
+  
+  
 
   // https://stackoverflow.com/a/28318964
   function parseFile(file, chunk, done) {
